f3a634b32003f89ddb3b41bf267fdb30032115d0
ARRYTHEBEAST <arjunkhamkar25@gmail.com>
2024-08-11 21:10:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+ // Map initialization 
+ var map = L.map('map').setView([16.686875, 74.2272],14);
+ var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+ osm.addTo(map);
+
+
+ var marker = L.marker([16.6868981, 74.2245119]).addTo(map); // ShivaMandir 1
+ var marker = L.marker([16.6878053, 74.2268129]).addTo(map); // RadhaKrishna Mandir Old Maratha Architecture
+ var marker = L.marker([16.6876496, 74.2272880]).addTo(map); // Padmavati Mandir
+ var marker = L.marker([16.6880179, 74.2272511]).addTo(map); // GajaGanesh
+ var marker = L.marker([16.6875259, 74.2285171]).addTo(map); // Jayprabha Studio
+ var marker = L.marker([16.6840401, 74.2319141]).addTo(map); // Renuka Mandir s.1840
+ var marker = L.marker([16.692538, 74.224976]).addTo(map); //Mirajkar Tikti
+ var marker = L.marker([16.692193, 74.224434]).addTo(map); //Nurshimha Mandir 1
+ var marker = L.marker([16.6918619,74.2243293]).addTo(map); //Onkareshwar Mandir Southern Script(raja Bhoj?)
+ var marker = L.marker([16.6916495, 74.2307380]).addTo(map); //Sathmari
+ var marker = L.marker([16.690646, 74.229167]).addTo(map); //Belbag Chowk
+
+
+

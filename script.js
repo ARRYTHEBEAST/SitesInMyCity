414dfcf5c2292eafb10c48b392fd81003b26bc2d
ARRYTHEBEAST <arjunkhamkar25@gmail.com>
2024-08-12 13:27:53 +0530

    adding more places

--- script.js
+++ script.js
@@ -15,6 +15,14 @@
  var marker = L.marker([16.6918619,74.2243293]).addTo(map); //Onkareshwar Mandir Southern Script(raja Bhoj?)
  var marker = L.marker([16.6916495, 74.2307380]).addTo(map); //Sathmari
  var marker = L.marker([16.690646, 74.229167]).addTo(map); //Belbag Chowk
+ var marker = L.marker([16.6923981, 74.2202060]).addTo(map); //ardha shivaji putala
+ var marker = L.marker([16.6929595, 74.2198519]).addTo(map); //Brahmeshwar Park
+ var marker = L.marker([16.6929733, 74.2182406]).addTo(map); //Mahakali mandir 1
+ var marker = L.marker([16.6944673, 74.2143604]).addTo(map); //rankala tower
+ var marker = L.marker([16.6994986, 74.2117278]).addTo(map); //anugamini mandir
+ var marker = L.marker([16.6966896, 74.2139182]).addTo(map); //Old Washing Place (dhobi)
+ var marker = L.marker([16.6999160, 74.1945288]).addTo(map); //Laksheswar Mandir(waterfront)
+ 
 
 
 
